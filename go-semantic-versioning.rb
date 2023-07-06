53d84fde2c3e2fbcbb533baa1d13a353a3e4b2c1
wpartin <djwest91@gmail.com>
2023-07-06 09:17:32 -0500

    [master] updated .rb file

--- go-semantic-versioning.rb
+++ go-semantic-versioning.rb
@@ -33,7 +33,7 @@ class GoSemanticVersioning < Formula
       sha256 "e7e74fcca1d143a6b56b67c287e9a6825dbef5fab0d9dceae392fbcc75ce7482"
 
       def install
-        bin.install "go-semantic-release.go"
+        bin.install "go-semantic-release"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
