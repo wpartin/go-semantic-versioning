7af4a08747aa44ca66cb8ddb34e00f2eec073861
wpartin <djwest91@gmail.com>
2023-07-11 08:07:09 -0500

    [master] added space before current version

--- pkg/printCurrentVersion.go
+++ pkg/printCurrentVersion.go
@@ -8,5 +8,6 @@ import (
 func PrintCurrentVersion () {
 	output, _ := exec.Command("brew", "list", "--versions", "go-semantic-versioning").Output()
 
+	fmt.Println()
 	fmt.Println(string(output))
 }
