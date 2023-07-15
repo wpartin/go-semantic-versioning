fef74afd6b10eb487c3f2514fe41e856b9eb96d9
wpartin <djwest91@gmail.com>
2023-07-15 07:44:05 -0500

    [master] updated release to print output

--- pkg/createRelease.go
+++ pkg/createRelease.go
@@ -1,6 +1,7 @@
 package pkg
 
 import (
+	"bufio"
 	"fmt"
 	"os/exec"
 )
@@ -9,5 +10,33 @@ func CreateRelease (choice bool, version string) {
 	fmt.Printf("Creating release for %s.", version)
 	fmt.Println()
 
-	exec.Command("goreleaser", "release", "--clean").Run()
+	cmd := exec.Command("goreleaser", "release", "--clean")
+
+	stderr,	err := cmd.StderrPipe()
+	stdout, outErr := cmd.StdoutPipe()
+
+	if err != nil {
+		panic(err)
+	}
+
+	if outErr != nil {
+		panic(outErr)
+	}
+
+	cmd.Start()
+	
+	scannerErr := bufio.NewScanner(stderr)
+	scannerOut := bufio.NewScanner(stdout)
+
+	for scannerErr.Scan() {
+		message := scannerErr.Text()
+		fmt.Println(message)
+	}
+
+	for scannerOut.Scan() {
+		message := scannerOut.Text()
+		fmt.Println(message)
+	}
+
+	cmd.Wait()
 }
