4507c98d435e723d6d8ee7d4294158023bdf88e0
wpartin <djwest91@gmail.com>
2023-09-19 10:11:57 -0500

    made gh release specific

--- cmd/go-semantic-versioning.go
+++ cmd/go-semantic-versioning.go
@@ -8,10 +8,10 @@ import (
 )
 
 func main () {
-	branch  := flag.String("branch", "", "[REQUIRED] The branch to initate versioning from; release, feature, bugfix, hotfix.")
-	release := flag.Bool("release", false, "[OPTIONAL] Create a GitHub Release with goreleaser. Must have goreleaser installed for this to work.")
-	tag     := flag.String("tag", "", "[OPTIONAL] The tag for a previous release that you want to increment. Must use the following pattern: \"v#.#.#\"")
-	version := flag.Bool("version", false, "[OPTIONAL] Check the current version.")
+	branch     := flag.String("branch", "", "[REQUIRED] The branch to initate versioning from; release, feature, bugfix, hotfix.")
+	gh_release := flag.Bool("release", false, "[OPTIONAL] Create a GitHub Release with goreleaser. Must have goreleaser installed for this to work.")
+	tag        := flag.String("tag", "", "[OPTIONAL] The tag for a previous release that you want to increment. Must use the following pattern: \"v#.#.#\"")
+	version    := flag.Bool("version", false, "[OPTIONAL] Check the current version.")
 
 	flag.Parse()
 
@@ -25,6 +25,6 @@ func main () {
 
 		version := pkg.Version(*branch, *tag)
 
-		if *release || *branch == "release" { pkg.CreateRelease(version) }
+		if *gh_release { pkg.CreateRelease(version) }
 	}
 }
